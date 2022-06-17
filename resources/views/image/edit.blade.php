83001515f88cf2f8ae9a0928e9a3e7ee6702373f
Osadiaye Solomon <srose033@gmail.com>
2022-06-17 03:48:04 +0100

    updates

--- resources/views/image/edit.blade.php
+++ resources/views/image/edit.blade.php
@@ -1,13 +1,11 @@
 <h1>Edit image</h1>
 
-<form action="{{ $image->route('update') }}" method="POST" >
- @csrf
- @method('PUT')
+<x-form action="{{ $image->route('update') }}" method="PUT">
+  
  <div>
      <img src="{{$image->fileUrl()}}" alt="{{$image->title}}" width="400">
  </div>
  
-
  <div>
     <label for="title">Title</label>
     <input type="text" name="title" id="title" value="{{old('title', $image->title)}}">
@@ -17,4 +15,4 @@
 </div>
 
 <button type="submit">Update</button>
-</form>
+</x-form>
--- resources/views/image/index.blade.php
+++ resources/views/image/index.blade.php
@@ -1,25 +1,25 @@
-<h1>All images</h1>
-
-<a href="{{route('images.create')}}">Upload Image</a>
-
-@if($message = session('message'))
-  <div>{{$message}}</div> 
-@endif
-
-@foreach ($images as $image)
-  <div>
-      <a href="{{ $image->permalink() }}">
-          <img src="{{$image->fileUrl()}}" alt="{{$image->title}}" width="300">
-      </a>
-      <div>
-          <a href="{{$image->route('edit')}}">Edit</a>
-          <form action="{{$image->route('destroy')}}" method="POST" style="display: inline">
-          @csrf
-          @method('DELETE')
-          <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
-          </form>
-
-      </div>
-  </div>
-
-@endforeach
+<x-layout title="Discover free images">
+  <h1>All images</h1>
+  
+  <a href="{{route('images.create')}}">Upload Image</a>
+  
+  @if($message = session('message'))
+    <div>{{$message}}</div>
+  @endif
+  
+  @foreach ($images as $image)
+    <div>
+        <a href="{{ $image->permalink() }}">
+            <img src="{{$image->fileUrl()}}" alt="{{$image->title}}" width="300">
+        </a>
+        <div>
+            <a href="{{$image->route('edit')}}">Edit</a>
+            <x-form action="{{$image->route('destroy')}}" method="DELETE" style="display: inline">
+            <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
+            </x-form>
+  
+        </div>
+    </div>
+  
+  @endforeach
+</x-layout>
